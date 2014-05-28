43b8c7e90ae49dc6f2804a046c255a3f81634851
Brendan O'Connor <brenocon@gmail.com>
2014-05-28 13:23:47 -0400

    friendlier usage help for setvenn

--- setvenn.py
+++ setvenn.py
@@ -1,23 +1,47 @@
 #!/usr/bin/env python
-"""setvenn  <set1> <set2>
+"""setvenn [opts] <set1> <set2>
 Venn diagram overview of two files as sets.  
 Show interesting counts and Jaccard ratio.
 -s : Show items of the set differences.
 
 We don't newline chomp, so a bug if your file doesnt end with a newline 
 Dash - for stdin (e.g. cut/awk/sed/grep)
-Though in zsh, =(cmd) syntax is superior: can do 2 pipeline inputs
+or try <(cmd) or =(cmd) shell syntax
+
+EXAMPLE
+
+  $ setvenn list1.txt list2.txt
+    |A| =    28   |A&B| =    22     |B| =    22
+  |A-B| =     6   |AvB| =    28   |B-A| =     0
+     AB/A 0.786    Jacc = 0.786      AB/B 1.000
+
+  $ setvenn -s list1.txt list2.txt 
+    |A| =    28   |A&B| =    22     |B| =    22
+  |A-B| =     6   |AvB| =    28   |B-A| =     0
+     AB/A 0.786    Jacc = 0.786      AB/B 1.000
+
+  ***  |A-B| =  6  ***
+  APW_ENG_20030122.0094.ann
+  APW_ENG_19960124.0119.ann
+  APW_ENG_20030803.0091.ann
+  APW_ENG_19960322.0777.ann
+  APW_ENG_20081024.0398.ann
+  APW_ENG_20001229.0811.ann
+
+  ***  |B-A| =  0  ***
 """
 
 from __future__ import division
 import sys
+
 show_items=False
 if '-s' in sys.argv:
   sys.argv.pop( sys.argv.index('-s') )
   show_items = True
-if len(sys.argv) == 1:
+if len(sys.argv) != 3 or '-h' in sys.argv:
   print __doc__.strip()
   sys.exit(1)
+
 file1,file2 = sys.argv[1], sys.argv[2]
 file1 = sys.stdin if file1=='-' else open(file1)
 file2 = sys.stdin if file2=='-' else open(file2)
@@ -34,7 +58,7 @@ print "   AB/A %-*.3f    Jacc = %-*.3f      AB/B %-*.3f" % (
     w,len(_and)/len(_or) if len(_or) else float('nan'),
     w,len(_and)/len(b) if len(b) else float('nan'))
 if show_items:
-  print "\n***  |A-B| = %*d  ***" % (w,len(a-b),)
+  print "\n***  |A-B| = %2d  ***" % (len(a-b),)
   for x in (a-b): print x,
-  print "\n***  |B-A| = %*d  ***" % (w,len(b-a),)
+  print "\n***  |B-A| = %2d  ***" % (len(b-a),)
   for x in (b-a): print x,
